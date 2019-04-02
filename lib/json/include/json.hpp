c58446163cdfca5f86a068030317e0aa07fbaab2
Junsuk Park <park64kr63@gmail.com>
2019-04-02 10:31:48 +0900

    # REFACTOR - json.hpp
    ## Description
    - Upgrade nlohmann/json from 3.4.0 to 3.6.1
    - Replace `get~~` with a template function `get`
    - `getString` is redundant.

--- lib/json/include/json.hpp
+++ lib/json/include/json.hpp
@@ -2,6 +2,7 @@
 
 #include "../../../include/json.hpp"
 #include <optional>
+#include <type_traits>
 
 using namespace std;
 
@@ -14,46 +15,16 @@ static auto is_empty(const nlohmann::json &j) {
   return j.is_discarded();
 }
 
-static optional<string> getString(const nlohmann::json &json_obj) {
-  std::string ret_str;
-  if (!json_obj.empty()) {
-    try {
-      return json_obj.get<std::string>();
-    } catch (...) {
-      /* do nothing */
-    }
-  }
-  return {};
-}
-
-static optional<string> getString(const nlohmann::json &json_obj, const std::string &key) {
-  if (json_obj.find(key) != json_obj.end()) {
-    return getString(json_obj[key]);
-  }
-  return {};
-}
-
-static optional<int> getInt(nlohmann::json &json_obj, const string &key) {
-  auto dec_str = getString(json_obj, key);
-  if (dec_str.has_value())
-    return stoi(dec_str.value());
-  return {};
-}
-
-static optional<uint64_t> getUint64(nlohmann::json &json_obj, const string &key) {
-  auto dec_str = getString(json_obj, key);
-  if (dec_str.has_value())
-    return static_cast<uint64_t>(stoll(dec_str.value()));
-  return {};
-}
-
-static optional<bool> getBoolean(const nlohmann::json &json_obj, const std::string &key) {
-  if (json_obj.find(key) != json_obj.end()) {
-    if (!json_obj[key].is_boolean()) {
+template <typename T>
+static optional<T> get(const nlohmann::json &json_obj, const string &key) {
+  if (json_obj.contains(key)) {
+    if (is_convertible_v<decltype(json_obj[key]), T>) {
+      return static_cast<T>(json_obj[key]);
+    } else {
       return {};
     }
-    return json_obj[key].get<bool>();
+  } else {
+    return {};
   }
-  return {};
 }
 } // namespace json
