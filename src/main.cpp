e5de9255b36cce5119645d65ee2dcd2003d4fa16
Junsuk Park <park64kr63@gmail.com>
2019-05-14 15:38:04 +0900

    # EDIT - CheckStatus 동작하도록 수정
    ## 수정사항
    - ip/port -> address 로 통일
    - `void AdminService<ReqStatus, ResStatus>::proceed()`  구현

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,14 @@
 #include "plugins/block_producer_plugin/include/block_producer_plugin.hpp"
 #include "plugins/chain_plugin/include/chain_plugin.hpp"
 #include "plugins/net_plugin/include/net_plugin.hpp"
+#include "plugins/admin_plugin/include/admin_plugin.hpp"
 #include <iostream>
 
 using namespace std;
 using namespace gruut;
 
 int main(int argc, char **argv) {
-  if (!appbase::app().initialize<NetPlugin, ChainPlugin, BlockProducerPlugin>(argc, argv))
+  if (!appbase::app().initialize<AdminPlugin, NetPlugin, ChainPlugin, BlockProducerPlugin>(argc, argv))
     return -1;
 
   appbase::app().start();
--- src/plugins/admin_plugin/rpc_services/rpc_services.cpp
+++ src/plugins/admin_plugin/rpc_services/rpc_services.cpp
@@ -9,29 +9,37 @@ namespace admin_plugin {
 using namespace appbase;
 using namespace std;
 
-//TODO : handle admin request
-template<>
-void AdminService<ReqSetup, ResSetup>::proceed() {
+// TODO : handle admin request
+template <>
+void AdminService<ReqSetup, ResSetup>::proceed() {}
 
+template <>
+void AdminService<ReqStart, ResStart>::proceed() {}
+
+template <>
+void AdminService<ReqStop, ResStop>::proceed() {}
+
+template <>
+void AdminService<ReqStatus, ResStatus>::proceed() {
+  switch (receive_status) {
+  case AdminRpcCallStatus::CREATE: {
+    receive_status = AdminRpcCallStatus::PROCESS;
+    service->RequestCheckStatus(&context, &req, &responder, completion_queue, completion_queue, this);
+  } break;
+
+  case AdminRpcCallStatus::PROCESS: {
+    new AdminService(service, completion_queue);
+
+    res.set_alive(true);
+
+    Status rpc_status = Status::OK;
+    receive_status = AdminRpcCallStatus::FINISH;
+    responder.Finish(res, Status::OK, this);
+  } break;
+
+  default: { delete this; } break;
+  }
 }
-
-template<>
-void AdminService<ReqStart, ResStart>::proceed() {
-
-}
-
-template<>
-void AdminService<ReqStop, ResStop>::proceed(){
-
-}
-
-template<>
-void AdminService<ReqStatus, ResStatus>::proceed(){
-
-}
-
-}
-
 
 } // namespace admin_plugin
 } // namespace gruut
