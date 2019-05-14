f88d4151f4c439a85806fcda0426d747183463ad
blackiron <jjj@gruut.net>
2019-05-14 09:36:47 +0900

    # ADD - Admin Service Rpc

---
+++ src/plugins/admin_plugin/rpc_services/rpc_services.cpp
@@ -0,0 +1,37 @@
+#include "include/rpc_services.hpp"
+#include "../../../../lib/json/include/json.hpp"
+
+#include <exception>
+
+namespace gruut {
+namespace admin_plugin {
+
+using namespace appbase;
+using namespace std;
+
+//TODO : handle admin request
+template<>
+void AdminService<ReqSetup, ResSetup>::proceed() {
+
+}
+
+template<>
+void AdminService<ReqStart, ResStart>::proceed() {
+
+}
+
+template<>
+void AdminService<ReqStop, ResStop>::proceed(){
+
+}
+
+template<>
+void AdminService<ReqStatus, ResStatus>::proceed(){
+
+}
+
+}
+
+
+} // namespace admin_plugin
+} // namespace gruut
